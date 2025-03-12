bf5289f6838ad5136bd8cf36c846759940fc87c2
MBPCODE2002 <mbpfocus21@gmail.com>
2025-03-12 10:53:33 +0530

    Create basic.java

---
+++ basic.java
@@ -0,0 +1,213 @@
+package p1;
+
+import java.lang.Math;
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		
+//		System.out.println("My name is Mitul..."); 
+//
+//		int a = 2002;
+//		String name ="mitul";
+//		
+//		boolean isAuth = true;
+//		
+//		
+//		float per = 200.56f;
+//		
+//		
+//		System.err.println(a + " " + name + " "+ isAuth + " " + per );
+//		
+//		
+//		
+//		
+//		int mitul = 45;
+//		
+//		int prajapati = 67;
+		
+		
+//		taking 3 varaiable we have to swap the value of mitul and prajapati
+//		
+//	 int temp = 0;
+//	 
+//	 temp = mitul;
+//	 mitul=prajapati;
+//	 
+//	 prajapati=temp;
+//	 
+//	 System.out.println("hello guys  after swapping" + "mitul value is" + mitul + "prajapati value is " +  prajapati );
+//	
+	 
+	 
+//		int	 a = 25;
+//		int b = 45;
+//		
+////		so without taking 3 variable we have to swap the value of a and b 
+//		
+//	a = a+b;
+//	b = a-b;
+//
+//	
+//	a = a-b;
+//	
+//	System.out.println( a + " " + b );
+//	
+//	
+//	
+//	
+//	String Name = "Mitul";
+//	
+//	int salary = 9099999;
+//
+//	int per_yearsalary;
+//	
+//	per_yearsalary= salary/12;
+//	
+//	
+//	System.out.println( per_yearsalary);
+//	
+//	
+//	
+//	
+//	
+//	 int r =25;
+//	 
+//	 a=r*r;
+//	 
+//	 
+//	 System.out.println(  " the area of circle is "+ 3.14*a);
+//	 
+////	 volume of circle 
+//	 
+////	 a= 4/3*3.14 r^2
+//	 
+//	 b=r*a;
+//	 
+//	 
+//	 
+//	 System.out.println(" the volume of circle is  " +(4/3)*3.14*b );
+//	 
+//	 
+//	 
+////	 c=(f-32/1.8) formula 
+//	 
+//	 
+//	 int faherenheit=55;
+//	 
+//	 int m= faherenheit-32;
+//	 
+//	 
+//	 System.out.println("celcius ="+ m/1.8);
+//	 
+//	 
+////	 f=(c*1.8)+32
+//	 
+//	 float h = 45.55f;
+//	 
+//	 float f = h*1.8f;
+//	 
+//	 System.out.println("celcius to f so f is "+ f+32);
+//	 
+//	 
+//	int side =3;
+//	
+//	int volume= side*3;
+//	
+//	System.out.println(" volume of square"+ " " + volume );
+//	 
+//	 
+//	 
+//	 
+		
+//		average of 3 number .........
+		
+//		
+//		int a = 5,b=2,c=4;
+//		float d = (float)(a+b+c)/3;
+//	  
+//	System.out.println("average of 3 number is " + d );
+	 
+
+
+		
+//	The formula is Interest = (Principal * Rate * Time) / 100
+//	
+//
+//	float p = 45.5f;
+//	float rate=99.9f;
+//	float time=55.45f;
+//	
+//	float he=p*rate*time/100;
+//	
+//	System.out.println("this the simple interest" + " " + he);
+//	
+	
+	
+	
+//	calculates the distance between two points (x1, y1) and (x2, y2) using the distance formula: Distance = √((x2 - x1)² + (y2 - y1)²)
+	
+//	float x1 = 45.6f,x2 = 55.67f,y1 = 65.55f,y2=34.55f;
+//	
+//	double a = (x2 -x1), b= a*a;
+//	
+//	System.out.println(b);
+//	
+//	double c =(y2-y1), d = c*c;
+//	
+//	System.out.println(d);
+//	
+//	
+//	double f = b+ d;
+//	
+//	System.out.println(f);
+//	
+//	 System.out.println(Math.sqrt(f));
+		
+		
+	 
+//	 a quadratic equation ax² + bx + c = 0
+	 
+//	 x = (-b ± √(b² - 4ac)) / 2a
+//	 
+//	 double a = 22.2,d=2*a;
+//	 double b =97.3,e=b*b;
+//	 double c=44.5,f=4*a*c;
+//	 
+//	 double m = e-f,i=Math.sqrt(m);
+//	 
+//	 double x=(-b +i)/d;
+//	 
+//	 System.out.println(x);
+	 
+//	 Use the semi-perimeter s = (a + b + c) / 2
+	 
+//		Heron's formula for the area: Area = sqrt(s * (s - a) * (s - b) * (s - c))
+		
+		int a=1,b=2,c=3,s=a+b+c/2;
+		
+		System.out.println("semi perimeter is" + s);
+
+		int f=s*(s-a)*(s-b)*(s-c);
+		
+	System.out.println(Math.sqrt(f));
+//		
+//		
+//		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+	
+
+}
+
+
+     
