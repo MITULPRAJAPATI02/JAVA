51f9327bf8dbf3a57b56639a95f253887d574c1f
MITUL PRAJAPATI <mbpfocus21@gmail.com>
2025-03-19 10:47:23 +0530

    Update loop.java

--- loop.java
+++ loop.java
@@ -42,3 +42,37 @@
 			 
 	
 			 else{System.out.println("it is not a Armstrong number ");}
+
+
+// hollow pattern
+	
+// 		for (int i=1;i<=5;i++)
+			
+// 		{
+// 			for (int k=5-i;k>0;k--)
+				
+				
+// 			{System.out.print(" ");}
+			
+			
+// 			for(int j=1;j<=2*i-1;j++)
+				
+				
+// 				if(j==1 || (j==2*i-1))
+					
+// 				{	System.out.print("*");}
+			
+// 				else{System.out.print(" ");}
+				
+// //			{System.out.print("*");}
+			
+// 			System.out.println();
+// 		}
+
+
+
+
+
+
+
+
