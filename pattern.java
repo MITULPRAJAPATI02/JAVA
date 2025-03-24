278d2d9db0f7fee417a76a80974da81c8d3dbf9b
MITUL PRAJAPATI <mbpfocus21@gmail.com>
2025-03-24 10:05:51 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -69,4 +69,19 @@ public class pattern {
 		      
 		      System.out.print("\n");
 		  }
-		  
+ 1 
+		  12 
+		  123 
+		  1234 
+		  12345 
+		   
+		   
+		    for(int i = 1;i<=5;i++)
+		  {
+		      for(int j = 1;j<=i;j++)
+		      {
+		          System.out.print(i);
+		      } 
+		      
+		      System.out.print("\n");
+		  }		  
