05a434ecca2a0aafe9ecc9f5aca89c0820dd513a
MITUL PRAJAPATI <mbpfocus21@gmail.com>
2025-03-25 17:25:25 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -54,34 +54,88 @@ public class pattern {
 // 	}
 // }
 
-                  * 
-		  ** 
-		  *** 
-		  **** 
-		  *****  
+    //               * 
+		  // ** 
+		  // *** 
+		  // **** 
+		  // *****  
 		  
-		  for(int i = 1;i<=5;i++)
-		  {
-		      for(int j = 1;j<=i;j++)
-		      {
-		          System.out.print("*");
-		      } 
+		  // for(int i = 1;i<=5;i++)
+		  // {
+		  //     for(int j = 1;j<=i;j++)
+		  //     {
+		  //         System.out.print("*");
+		  //     } 
 		      
-		      System.out.print("\n");
-		  }
- 1 
-		  12 
-		  123 
-		  1234 
-		  12345 
+		  //     System.out.print("\n");
+		  // }
+ //               1 
+	// 	  12 
+	// 	  123 
+	// 	  1234 
+	// 	  12345 
 		   
 		   
-		    for(int i = 1;i<=5;i++)
-		  {
-		      for(int j = 1;j<=i;j++)
-		      {
-		          System.out.print(i);
-		      } 
+	// 	    for(int i = 1;i<=5;i++)
+	// 	  {
+	// 	      for(int j = 1;j<=i;j++)
+	// 	      {
+	// 	          System.out.print(i);
+	// 	      } 
 		      
-		      System.out.print("\n");
-		  }		  
+	// 	      System.out.print("\n");
+	// 	  }		  
+
+
+
+
+
+
+		  //     * 
+		  //    ** 
+		  //   ***
+		  //  **** 
+		  // *****   
+		  
+//		  for(int i =1;i<=5;i++)
+//		  {
+//		      for(int k = 1;k<=5-i;k++)
+//		      {
+//		          System.out.print(" ");
+//		      }
+//		      for(int j = 1;j<=i;j++)
+//		      {
+//		          System.out.print("*");
+//		      } 
+//		      
+//		      
+//		      System.out.println();
+//		  }
+		       
+		
+		
+		
+//		    1
+//		   22
+//		  333 
+//		 4444
+//		55555
+		
+	  for(int i =1;i<=5;i++)
+			  {
+			      for(int k=5-i;k>0;k--)
+			      {
+			          System.out.print(" ");
+			      }
+			      for(int j = 1;j<=i;j++)
+			      {
+			          System.out.print(i);
+			      } 
+			      			      
+			      System.out.println();
+			  }
+
+
+	}}
+
+	
