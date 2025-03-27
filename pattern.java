ac8bf0e9ca3f7c97711dc262bf42ae1b910058c6
MITUL PRAJAPATI <mbpfocus21@gmail.com>
2025-03-27 09:39:06 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -121,21 +121,48 @@ public class pattern {
 //		 4444
 //		55555
 		
-	  for(int i =1;i<=5;i++)
-			  {
-			      for(int k=5-i;k>0;k--)
-			      {
-			          System.out.print(" ");
-			      }
-			      for(int j = 1;j<=i;j++)
-			      {
-			          System.out.print(i);
-			      } 
+	  // for(int i =1;i<=5;i++)
+			//   {
+			//       for(int k=5-i;k>0;k--)
+			//       {
+			//           System.out.print(" ");
+			//       }
+			//       for(int j = 1;j<=i;j++)
+			//       {
+			//           System.out.print(i);
+			//       } 
 			      			      
-			      System.out.println();
-			  }
+			//       System.out.println();
+			//   }
 
+//****		
+// ****		
+//  ****		
+//    ****
 
+//butterfly pattern
+		
+		for(int i=1;i<=6;i++)
+			
+		{
+			
+			for(int j=1;j<=i;j++)
+				
+			{System.out.print("*");}  
+			
+			for(int k=1;k<=6-i;k++)
+			{System.out.print("  ");}
+			
+			for(int x=1;x<=i;x++)
+			{System.out.print("*");}
+			
+			
+			
+			
+			
+			System.out.println();
+			
+		} 
 	}}
 
 	
