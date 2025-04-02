ed50d78b4d640bba7c1da8cb60b9c65c4e29bd39
MITUL PRAJAPATI <mbpfocus21@gmail.com>
2025-04-02 16:28:04 +0530

    Create Basics.java

---
+++ OOPS/Basics.java
@@ -0,0 +1,61 @@
+
+
+public class Main
+{
+   
+	public static void main(String[] args) {
+		
+		
+		Student std = new Student(5); 
+				System.out.println(std.demo());  
+
+// 		std.demo();
+		
+		
+	}
+} 
+
+
+class Student 
+{ 
+    private int x;
+     public Student(int xdata)
+    {
+        x = xdata;
+    }
+    
+    public int demo()
+    { 
+        return this.x;
+        // 	System.out.println("Testing.." + this.x);
+    }
+}
+// OOPS: Object oriented Programming Structure 
+
+// C C++ 
+// C : Procedure Oriented 
+// C++ : OOP  
+
+// OOP is faster and easier to execute
+// OOP Provide cleare structure 
+// Don't Repete Your self 
+// reuse 
+// security  
+
+// Class 
+// Object 
+// Consctor : special type of function but it calls automaticaly when object created 
+
+
+// 1. Encapsulation : Wrapping up[ of the data in single unit : acess modifier : default,public,private,protected ]
+// 2. Inheritance 
+// 3. Abstraction 
+// 4. Polymorphisam 
+
+// class: collection of member function and data ..  student 
+// object: real tme entity 
+
+
+//  Create One Main Class And DateObj Class 
+//  Main Class Call DateObj  and  create 1 consctor in it and assign variable 
+//  create 1 function display and print variable name
