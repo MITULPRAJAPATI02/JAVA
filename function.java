c2e70cae09f8de91aa05ebd7a77ee387aacf8aee
MITUL PRAJAPATI <mbpfocus21@gmail.com>
2025-04-07 15:52:20 +0530

    Create function.java

---
+++ function.java
@@ -0,0 +1,276 @@
+package p1;
+
+import java.util.Scanner;
+
+
+
+public class function {
+ 
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+//					 for(int i =1;i<10;i++)
+//					 {
+//						 if(i==4)
+//						 {
+//							 break;
+//							 continue;
+//						 }
+//						 System.out.println(i);
+//					 } 
+//				Scanner obj = new Scanner(System.in); 
+//				System.out.println("Enter Name");
+//				String name = obj.nextLine(); 
+//				
+				
+//				System.out.println("Enter Age");
+//				int age = obj.nextInt();
+				
+//				demo(name,age); 
+//				demo();
+		
+//				demo();
+
+//				checkAge(age);
+//			} 
+//			
+//			static void demo(String name,int age)
+//			{
+//				System.out.println("Hello World" + name + "" + age);
+//				
+//			} 
+//			
+//			static void checkAge(int age)
+//			{
+//				if(age<18)
+//				{
+//					System.out.println("No u can not vote");
+//				}
+//				else
+//				{
+//					System.out.println("Yes");
+//				}
+			 
+
+		
+
+
+		// Take argu,emt email and password from the user and check login  
+
+		 // Factorial using function 
+		
+		//array -> email  check email is in or not
+	
+	
+	
+	
+//		Scanner obj = new Scanner(System.in); 
+//		System.out.println("enter the email");
+//		String email = obj.nextLine(); 
+//	
+//	
+//	   System.out.println("enter the password");
+//	   String password=obj.nextLine();
+//	
+//	   
+//	   checklogin(email,password);
+//	}
+//	   
+//	static void checklogin(String email,String password) {
+//		
+//		
+//		String validemail="user@gmail.com";
+//		String validpassword="password123";
+//		
+//		
+//		if (email.equals(validemail) && password.equals(validpassword)) {
+//			System.out.println("Login sucessfully");
+//		}
+//		else {
+//			
+//			
+//			System.out.println("error !!!1 you enter incorrect email and password");
+//		}
+//		
+		
+//		find factorial using function
+		
+	
+			
+			
+			
+			
+			
+			
+	
+	
+	
+	
+//	array of email and the user enter email is in it or not
+		
+Scanner obj = new Scanner(System.in);
+		
+		String[] emails = {"yoyo@example.com", "mitul@example.com", "pratz@example.com"};
+		
+		System.out.println("Enter your email: ");
+		String inputEmail = obj.nextLine();
+		int flag=0;
+	
+		for(int i=0;i<emails.length;i++)
+			
+		{
+			if(inputEmail.equals(emails[i]))
+			{
+//				System.out.println("Email found");
+				
+				flag=1;
+				
+				break;
+				
+			}
+			else {
+				
+			
+			
+				flag=0;
+				
+				
+			}
+		
+		
+		
+		}
+		if(flag==1) {
+		System.out.println("email found");
+		}
+		else {
+			
+			System.out.println("email not dound");
+		}
+//	}
+
+
+	
+	
+	
+	
+	}}
+
+
+package p1;
+
+import java.util.Scanner;
+
+public class functionpart2 {
+	static void factorial(int num) {
+		
+//		int i;
+//		int temp=1;
+//		
+//		for(i=1;i<=num;i++)
+//		{
+//			temp*=i;}
+//	
+//		System.out.println("factorial of" + num + "is:" + temp);}
+//	
+//	
+//	public static void main(String[] args) {
+//		
+//		
+//		
+//		Scanner obj=new Scanner(System.in);
+//		System.out.println("Enter the number of which you want to find factorial");
+//			int num=obj.nextInt();
+//
+//			factorial(num);
+//			
+//		// TODO Auto-generated method stub
+//			
+//			
+//			// Online Java Compiler
+//			// Use this editor to write, compile and run your Java code online
+
+//			class Main {
+//			    static int sumdata(int a,int b)
+//			    {
+//			        //  System.out.println("The Sum is "+ (a+b));  
+//			        return a+b;
+//			        
+//			    }
+//			    static double sumdata(double a,double b)
+//			    {
+//			         System.out.println("its in float function name ");
+//			        return a+b;
+//			    }
+//			    static String sumdata() 
+//			    {
+//			        return "hello";
+//			    } 
+//			    // recursion : function call itself
+//			    static int factorial(int no)
+//			    {
+//			        if(no<=1)
+//			        {
+//			            return 1;
+//			        }
+//			        else 
+//			        {
+//			            return no * factorial(no-1); 
+//			            
+//			                //   5 * factorial(4) 
+//			                //   5 * 4 * factorial(3) 
+//			                //   5 * 4* 3* factorial(2) 
+//			                //   5*4*3*2*factorial(1) 
+//			                //   5*4*3*2*1
+//			                  
+//			        }
+//			    }
+//			    public static void main(String[] args) {
+//			        
+//			        // System.out.println(sumdata(10,20)); 
+//			        // System.out.println("Sum Data is" + sumdata(13.0,2.0));
+//			        // System.out.println(sumdata()); 
+//			        
+//			        System.out.println(factorial(5));
+//
+//			    }
+//			} 
+
+
+			// Function Overloading : same name but para meters or singnatures 
+
+			//  1. Using recursion sum kravano che ... take number from the user 
+			//  2. fibonachi series
+	
+		
+		
+		
+		
+		int a=34,b=24,c=13;
+		
+		 static void hello();
+		
+		
+		
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+
+	}
+
+}
+
+   
